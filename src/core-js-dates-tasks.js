a8c2fd96d9489d53c692d7b436cb353419815d0a
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2024-03-25 19:53:38 +0300

    complete 5th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -95,8 +95,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year, day = 0) {
+  const daysInMonth = new Date(year, month, day);
+  return daysInMonth.getDate();
 }
 
 /**
