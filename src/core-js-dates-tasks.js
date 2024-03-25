29e7aae8ec485cec755f9304c443def530e7084b
Daniyar Hapurzhonau <hapurzhonau@gmail.com>
2024-03-25 20:11:31 +0300

    feat: complete 6th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -111,8 +111,11 @@ function getCountDaysInMonth(month, year, day = 0) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const daysOnPeriod = end - start;
+  return daysOnPeriod / 86400000 + 1;
 }
 
 /**
